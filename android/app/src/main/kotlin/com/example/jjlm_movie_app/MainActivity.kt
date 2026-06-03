8ec9a3113d3d2ad1d002787f289191ce26d18a04
Loredo31 <31loredo@gmail.com>
2026-06-02 18:59:59 -0600

    init: Configuración del proyecto inicial

---
+++ android/app/src/main/kotlin/com/example/jjlm_movie_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jjlm_movie_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:jjlm_movie_app/config/theme/app_theme.dart';
+import 'presentation/screens/screens.dart';
+
+
+void main() {
+  runApp(
+    ProviderScope(child: const MainApp(),
+    )
+  );
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+    );
+  }
+}
